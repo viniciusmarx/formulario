c4e49f7aad4f81c898123e1dada3262d88bf8b67
vinicius marx <vinimarx@live.com>
2024-10-07 18:22:13 -0300

    Ajustar lógica

--- src/js/script.js
+++ src/js/script.js
@@ -5,22 +5,21 @@ const mensagem = document.createElement("p");
 mensagem.textContent = "campo obrigatório";
 mensagem.classList.add("aviso");
 
-botao.addEventListener("click", () => {
+botao.addEventListener("click", (event) => {
+	event.preventDefault();
 	for (let i = 0; i < inputs.length; i++) {
 		if (inputs[i].value == "") {
-			if (inputs[i].classList.contains("invalido")) {
-				continue;
-			} else {
+			if (!inputs[i].classList.contains("invalido")) {
 				inputs[i].classList.remove("valido");
 				inputs[i].classList.add("invalido");
 				adicionarMensagem(i);
 			}
 		} else {
-			inputs[i].classList.remove("invalido");
-			inputs[i].classList.add("valido");
-			if (containersInputs[i].classList.contains(mensagem)) {
+			if (inputs[i].classList.contains("invalido")) {
 				containersInputs[i].querySelector(".aviso").remove();
 			}
+			inputs[i].classList.remove("invalido");
+			inputs[i].classList.add("valido");
 		}
 	}
 });
